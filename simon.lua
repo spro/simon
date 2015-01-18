ae6e2dd47924e3a25a4724c2358554939b3feb47
Sean Robertson <sprobertson@gmail.com>
2015-01-17 18:03:34 -0800

    cookie module was failing randomly so read directly from ngx.vars

--- simon.lua
+++ simon.lua
@@ -1,23 +1,31 @@
-local cookie = require 'cookie'
+-- Connect to Redis
 local redis = require 'redis'
-
-local ck = cookie:new()
 local red = redis:new()
 red:connect('127.0.0.1', 6379)
 
-local sid, err = ck:get('connect.sid')
+-- Get session ID from cookie
+local sid = ngx.var['cookie_connect.sid']
+
 if sid then
+
+    -- Check for an assigned backend
     local backend, err = red:get('sid:' .. sid)
+
+    -- Use the assigned backend if it exists
     if backend ~= ngx.null then
         ngx.var.proxy_to = backend
         return
     end
+
 end
 
--- Get new one
+-- Choose a random backend based on the host
 local headers = ngx.req.get_headers()
 local backends = red:smembers('backends:' .. headers['host'])
 local chosen = backends[math.random(#backends)]
-ngx.var.proxy_to = chosen
+
 if sid then red:set('sid:' .. sid, chosen) end
+
+-- Set chosen proxy server
+ngx.var.proxy_to = chosen
 
