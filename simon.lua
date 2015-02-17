0f67913d30bf6eba51f571e3055cbb4185c280ca
Sean Robertson <sprobertson@gmail.com>
2015-02-17 15:18:08 -0800

    use host as part of session key to prevent redirecting to the wrong subdomain

--- simon.lua
+++ simon.lua
@@ -5,11 +5,12 @@ red:connect('127.0.0.1', 6379)
 
 -- Get session ID from cookie
 local sid = ngx.var['cookie_connect.sid']
+local headers = ngx.req.get_headers()
 
 if sid then
 
     -- Check for an assigned backend
-    local backend, err = red:get('sid:' .. sid)
+    local backend, err = red:get('sess:' .. headers['host'] .. ':' .. sid)
 
     -- Use the assigned backend if it exists
     if backend ~= ngx.null then
@@ -20,11 +21,10 @@ if sid then
 end
 
 -- Choose a random backend based on the host
-local headers = ngx.req.get_headers()
 local backends = red:smembers('backends:' .. headers['host'])
 local chosen = backends[math.random(#backends)]
 
-if sid then red:set('sid:' .. sid, chosen) end
+if sid then red:set('sess:' .. headers['host'] .. ':' .. sid, chosen) end
 
 -- Set chosen proxy server
 ngx.var.proxy_to = chosen
