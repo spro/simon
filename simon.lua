0c1f4529e5365e77d64ef05e2b56ee0b25eed345
Sean Robertson <sprobertson@gmail.com>
2015-02-19 22:02:05 -0800

    make host a local for later use

--- simon.lua
+++ simon.lua
@@ -3,14 +3,15 @@ local redis = require 'redis'
 local red = redis:new()
 red:connect('127.0.0.1', 6379)
 
--- Get session ID from cookie
+-- Get session ID from cookie and host from headers
 local sid = ngx.var['cookie_connect.sid']
 local headers = ngx.req.get_headers()
+local host  = headers['host']
 
 if sid then
 
     -- Check for an assigned backend
-    local backend, err = red:get('sess:' .. headers['host'] .. ':' .. sid)
+    local backend, err = red:get('sess:' .. host .. ':' .. sid)
 
     -- Use the assigned backend if it exists
     if backend ~= ngx.null then
@@ -21,10 +22,10 @@ if sid then
 end
 
 -- Choose a random backend based on the host
-local backends = red:smembers('backends:' .. headers['host'])
+local backends = red:smembers('backends:' .. host)
 local chosen = backends[math.random(#backends)]
 
-if sid then red:set('sess:' .. headers['host'] .. ':' .. sid, chosen) end
+if sid then red:set('sess:' .. host .. ':' .. sid, chosen) end
 
 -- Set chosen proxy server
 ngx.var.proxy_to = chosen
